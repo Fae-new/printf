d02d4e8ebd26ddd2a8137a4ee9bf7bc0ea31ac1f
Faluyi Ajibola <faluyiaji@gmail.com>
2023-10-16 18:52:14 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,90 +1,5 @@
 #include "main.h"
 
-
-
-/**
- * printstr - Prints a string to the standard output
- * @args: A va_list containing the string to be printed
- *
- * Description: This function takes a va_list as an argument,
- * interprets it as a string, and prints the string to the standard output.
- *
- * Return: The number of characters printed (excluding the null byte).
- *         Returns 0 if the va_list does not contain a valid string.
- */
-
-
-
-int printstr(va_list args)
-{
-	int count = 0;
-
-	char *str = va_arg(args, char *);
-
-	int len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-	write(STDOUT_FILENO, str, len);
-	count += len;
-	return (count);
-}
-
-/**
- * print - Prints formatted data to the standard output using a va_list
- * @format: A pointer to a format string containing format specifiers
- * @args: A va_list containing the arguments to be formatted and printed
- *
- * Description: This function takes a format string with format specifiers
- * and a va_list of arguments. It interprets the format specifiers and prints
- * the formatted output to the standard output.
- *
- * Return: The number of characters printed (excluding the null byte).
- *  Returns -1 if format is NULL or an invalid format specifier is found.
- */
-
-
-int print(const char *format, va_list args)
-{
-	int count = 0;
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-
-			if (*format == 'c')
-			{
-				char ch = va_arg(args, int);
-
-				write(STDOUT_FILENO, &ch, 1);
-				count++;
-			}
-			else if (*format == 's')
-				count += printstr(args);
-
-			else if (*format == '%')
-			{
-				write(STDOUT_FILENO, "%", 1);
-				count++;
-			}
-			else
-				return (-1);
-		}
-		else
-		{
-			write(STDOUT_FILENO, format, 1);
-			count++;
-		}
-		format++;
-	}
-
-	return (count);
-}
-
 /**
  * _printf - Prints formatted data to the standard output
  * @format: A pointer to a format string containing format specifiers
@@ -95,9 +10,8 @@ int print(const char *format, va_list args)
  * prints the formatted output to the standard output.
  *
  * Return: The number of characters printed (excluding the null byte).
- *   Returns -1 if format is NULL or an invalid format specifier is found.
+ *         Returns -1 if format is NULL or an invalid format specifier is found.
  */
-
 
 int _printf(const char *format, ...)
 {
@@ -116,3 +30,4 @@ int _printf(const char *format, ...)
 
 	return (count);
 }
+
