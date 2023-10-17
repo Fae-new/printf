0f65de487e1db19c35a2cbc5ee9a284cce7410a7
Faluyi Ajibola <faluyiaji@gmail.com>
2023-10-18 01:05:29 +0200

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+void print_binary(unsigned int num);
 char *int_to_string(int num);
 int printstr(char *str);
 int print(const char *format, va_list args);
