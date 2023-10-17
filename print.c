9cb7de70c53c7dbd57445a6770dd6d195e6957d4
Faluyi Ajibola <faluyiaji@gmail.com>
2023-10-18 00:27:15 +0200

    Update print.c

--- print.c
+++ print.c
@@ -10,41 +10,63 @@
  * the formatted output to the standard output.
  *
  * Return: The number of characters printed (excluding the null byte).
- * Returns -1 if format is NULL or an invalid format specifier is found.
+ *         Returns -1 if format is NULL or an invalid format specifier is found.
  */
+
 int print(const char *format, va_list args)
 {
-int count = 0;
+    int count = 0;
 
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-char ch = va_arg(args, int);
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
 
-write(STDOUT_FILENO, &ch, 1);
-count++;
-}
-else if (*format == 's')
-count += printstr(args);
+            if (*format == 'c')
+            {
+                char ch = va_arg(args, int);
 
-else if (*format == '%')
-{
-write(STDOUT_FILENO, "%", 1);
-count++;
-}
-else
-return (-1);
-}
-else
-{
-write(STDOUT_FILENO, format, 1);
-count++;
-}
-format++;
-}
-return (count);
+                write(STDOUT_FILENO, &ch, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *string = va_arg(args, char *);
+                count += printstr(string);
+            }
+
+            else if (*format == '%')
+            {
+                write(STDOUT_FILENO, "%", 1);
+                count++;
+            }
+
+            else if (*format == 'd' || *format == 'i')
+            {
+
+                int num = va_arg(args, int);
+                char *str = int_to_string(num);
+                if (str != NULL)
+                {
+                    count += printstr(str);
+                    free(str);
+                }
+                else
+                {
+                    return (-1);
+                }
+            }
+            else
+                return (-1);
+        }
+        else
+        {
+            write(STDOUT_FILENO, format, 1);
+            count++;
+        }
+        format++;
+    }
+
+    return (count);
 }
